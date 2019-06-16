2c58a638dc2a50e9d588011ead2deaca2652b9b7
Aaron Miller <aaronmiller822028@gmail.com>
2019-06-15 22:38:49 -0700

    the sign in form is looking great! possibly the best web design i have ever made

--- src/components/LandingPages/Signin.js
+++ src/components/LandingPages/Signin.js
@@ -1,30 +1,69 @@
 import React from "react";
 import './Signin.scss'
 import TextField from '@material-ui/core/TextField';
-import { makeStyles } from '@material-ui/styles';
+import Checkbox from '@material-ui/core/Checkbox';
+import {withStyles, makeStyles,} from '@material-ui/styles';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
 import Button from '@material-ui/core/Button';
+import LockIcon from '@material-ui/icons/Lock'
+import DeleteIcon from '@material-ui/icons/Delete';
 
-const useStyles = makeStyles({
+const CssTextField = withStyles({
     root: {
-        background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
-        border: 0,
-        borderRadius: 3,
-        boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
-        color: 'white',
-        height: 48,
-        padding: '0 30px',
+        '& label.Mui-focused': {
+            color: '#DE6D43',
+        },
+        '& .MuiOutlinedInput-root': {
+            '& fieldset': {
+                borderColor: '#54B0F2',
+            },
+            '&:hover fieldset': {
+                borderColor: '#003142',
+            },
+            '&.Mui-focused fieldset': {
+                borderColor: '#DE6D43',
+            },
+        },
     },
+})(TextField);
+
+const checkBoxStyles = theme => ({
+    root: {
+        '&$checked': {
+            color: '#54B0F2',
+        },
+    },
+    checked: {},
 });
+const CustomCheckbox = withStyles(checkBoxStyles)(Checkbox);
+
+const buttonStyle = makeStyles(theme => ({
+    button: {
+        width: '100%',
+        background:'#54B0F2',
+        '&:hover':{
+            background: '#DE6D43'
+        }
+    },
+    rightIcon: {
+        fontSize: 20,
+    },
+}));
 
 export default function Signin() {
-    const classes = useStyles();
+    const [state, setState] = React.useState({
+        checkedA: false,
+    });
 
-    var materialButtonStyle = {
-        width: '80%'
+    const handleChange = name => event => {
+        setState({ ...state, [name]: event.target.checked });
     };
+
+    const classes = buttonStyle();
 
     return(
         <div className="sign-section">
+
             <div className="sign-in">
                 Sign in
             </div>
@@ -34,20 +73,50 @@ export default function Signin() {
             <div className="learn-more">
                 Learn More
             </div>
-            <hr/>
-            <TextField
-                variant="outlined"
-                margin="normal"
-                required
-                style={materialButtonStyle}
-                id="email"
-                label="Email Address"
-                name="email"
-                autoComplete="email"
-                autoFocus
-                className="text-field"
-            />
-            <Button className={classes.root}>Hook</Button>
+            <div className="flex-form">
+                <CssTextField
+                    variant="outlined"
+                    margin="normal"
+                    required
+                    fullWidth
+                    id="email"
+                    label="Email Address"
+                    name="email"
+                    autoComplete="email"/>
+                <CssTextField
+                    variant="outlined"
+                    margin="normal"
+                    required
+                    fullWidth
+                    id="password"
+                    label="Password"
+                    name="password"
+                    autoComplete="current-password"/>
+
+                <FormControlLabel
+                    value="end"
+                    control={<CustomCheckbox
+                        onChange={handleChange('checkedA')}
+                        checked={state.checkedA}/>}
+                    label="End"
+                    labelPlacement="end"
+                />
+
+                <Button variant="contained" color="secondary" className={classes.button}>
+                    Sign in
+                    <LockIcon className={classes.rightIcon} />
+                </Button>
+
+                <div className="account-help-link">
+                    Username or Password help?
+                </div>
+
+                <div className="terms-accept">
+                    By clicking Sign in, you agree to the
+                    DUPLICAGENT Terms of Service, DUPLICAGENT Terms of use
+                    and have read and acknowledged our Privacy Statement.
+                </div>
+            </div>
         </div>
     )
 }
