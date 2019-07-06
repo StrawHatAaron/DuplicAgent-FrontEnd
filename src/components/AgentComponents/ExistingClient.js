9d8eb4721eed94d161c656b3c0b003915b4435c0
Aaron Miller <aaronmiller822028@gmail.com>
2019-07-05 18:43:39 -0700

    i might want this code on ExistingClient.js again sometime

--- src/components/AgentComponents/ExistingClient.js
+++ src/components/AgentComponents/ExistingClient.js
@@ -12,13 +12,35 @@ export default function ExistingClient(){
 
     var [files, setFiles] = useState([{},])
     useEffect(() => {
+        // const getMoreFiles = async (cursor, callback) => {  
+        //     // request further files from where the previous call finished  
+        //     const response = await dbx.filesListFolderContinue({ cursor })
+        //     // if a callback is provided we call it  
+        //     if (callback) callback(response)
+        //     if (response.has_more) {  
+        //       // if there are more files, call getMoreFiles recursively,  
+        //       // providing the same callback.  
+        //       await getMoreFiles(response.cursor, callback)  
+        //     }  
+        // }
+
         async function fetchDpx() {  
             const response = await dbx.filesListFolder({  
                 path: '',   
-                limit: 8     
+                limit: 5     
             })
+
+            // We can perform a custom action with received files  
+            // processFiles(response.entries)
+            // if (response.has_more) {  
+            //     // provide a callback for the newly received entries   
+            //     // to be processed  
+            //     console.log('has more')
+            //     getMoreFiles(response.cursor, more => more.entries)  
+            // } 
             //note need to wait for DOM to refresh so 
             //logging it here will not show a change
+
             setFiles(response.entries)
         }    
         fetchDpx()
@@ -26,43 +48,29 @@ export default function ExistingClient(){
 
 
     const displayCustomers = files.map((file) => {
-        let thumbnail
         var tagType = file['.tag']
-        console.log(tagType)
-        if (tagType == 'folder'){
-            console.log("what is going on")
-            thumbnail = file.thumbnail
-            ? `data:image/jpeg;base64, ${file.thumbnail}`
-            : `data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIyNCIgaGVpZ2h0PSIyNCIgdmlld0JveD0iMCAwIDI0IDI0IiBmaWxsPSJub25lIiBzdHJva2U9IiNmZmYiIHN0cm9rZS13aWR0aD0iMiIgc3Ryb2tlLWxpbmVjYXA9InJvdW5kIiBzdHJva2UtbGluZWpvaW49InJvdW5kIiBjbGFzcz0iZmVhdGhlciBmZWF0aGVyLWZpbGUiPjxwYXRoIGQ9Ik0xMyAySDZhMiAyIDAgMCAwLTIgMnYxNmEyIDIgMCAwIDAgMiAyaDEyYTIgMiAwIDAgMCAyLTJWOXoiPjwvcGF0aD48cG9seWxpbmUgcG9pbnRzPSIxMyAyIDEzIDkgMjAgOSI+PC9wb2x5bGluZT48L3N2Zz4=`
-        } else {
-            console.log("what is going on2")
-            thumbnail = `data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIyNCIgaGVpZ2h0PSIyNCIgdmlld0JveD0iMCAwIDI0IDI0IiBmaWxsPSJub25lIiBzdHJva2U9IiNmZmYiIHN0cm9rZS13aWR0aD0iMiIgc3Ryb2tlLWxpbmVjYXA9InJvdW5kIiBzdHJva2UtbGluZWpvaW49InJvdW5kIiBjbGFzcz0iZmVhdGhlciBmZWF0aGVyLWZvbGRlciI+PHBhdGggZD0iTTIyIDE5YTIgMiAwIDAgMS0yIDJINGEyIDIgMCAwIDEtMi0yVjVhMiAyIDAgMCAxIDItMmg1bDIgM2g5YTIgMiAwIDAgMSAyIDJ6Ij48L3BhdGg+PC9zdmc+`
-        }
+        // console.log(tagType)
 
         const urlBaseLength = document.URL.split("/")[0].length +
                 document.URL.split("/")[2].length
         const initRoutingPath = document.URL.substring(urlBaseLength+2, document.URL.length)
-        const fileName = file.name.replace(/\s/g, '')
-
-        console.log(urlBaseLength)
-        console.log(document.URL.split("/"))
-        console.log(document.URL.substring(urlBaseLength+2, document.URL.length))
+        const fileName = (""+file.name).replace(/\s/g, '_')
+        // console.log(urlBaseLength)
+        // console.log(document.URL.split("/"))
+        // console.log(document.URL.substring(urlBaseLength+2, document.URL.length))
         return (
             <li key={file.id}>
             {/* <Router 
                 exact path={initRoutingPath+file.name.replace(/\s/g, '')}> */}
             <a href={initRoutingPath + "/" + fileName}>
-                
-                    <img 
-                        class="dbx-thumb" 
-                        src={thumbnail}
-                        alt="folder-thumbnail"/>
-                    {file.name}
+                {file.name}
             </a>
             {/* </Router> */}
             </li>
         )
     })
+
+
 
     return(
         <div className="existing-client">
