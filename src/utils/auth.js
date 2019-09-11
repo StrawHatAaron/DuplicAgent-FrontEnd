0522b95be4f3e82dde7080e4d33b798b57d1cacf
Aaron Miller <aaronmiller822028@gmail.com>
2019-09-11 14:26:52 -0700

    somehow I broke auth

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -1,28 +1,21 @@
 import axios from 'axios'
 
+const AuthTokenKey = "AuthToken"
+
 class Auth {
     constructor() {
       this.authenticated = false;
     }
   
-    login(cb) {
-      this.authenticated = true;
-      cb();
-    }
-  
-    logout(cb) {
-      this.authenticated = false;
-      cb();
-    }
-  
+    //not sure if this will be needed
     isAuthenticated() {
       return this.authenticated;
     }
 
     authenticate(emailValue, passwordValue, callback){
-        console.log("email value "+emailValue)
-        console.log("password value "+passwordValue)
-        axios.post('http://127.0.0.1:8000/api/user/token/', {
+        // console.log("email value "+emailValue)
+        // console.log("password value "+passwordValue)
+        axios.post('http://127.0.0.1:8000/api/user/me/', {
             headers: {
                 'Access-Control-Allow-Origin': '*',
                 'Content-Type': 'application/json',
@@ -32,12 +25,14 @@ class Auth {
         })
         .then((response) => {
             console.log(response)
-            console.log(response.status)
+            // console.log(response.status)
             if(response.status===200){
-                window.localStorage.setItem(true, "SignedIn")
-                console.log("does this happen first")
-                this.login(callback)
-                console.log("proper username and password info was submitted")
+                const token = response.data['token']
+                window.localStorage.setItem(token, AuthTokenKey)
+                console.log(token)
+                this.authenticated = true
+                //Use the callback function after setting variables
+                callback()
                 return true
             }
         }, (error) => {
@@ -46,6 +41,32 @@ class Auth {
         })
 
     }
+
+    checkAuthentication(){
+        const token = window.localStorage.getItem(AuthTokenKey)
+        axios.post('http://127.0.0.1:8000/api/user/token/', {
+            headers: {
+                'Access-Control-Allow-Origin': '*',
+                'Content-Type': 'application/json',
+                'Authorization': 'Token ' + token
+            },
+        })
+        .then((response) => {
+            console.log('----------------------------')
+            console.log('checkAuthentication')
+            console.log(response)
+            console.log('----------------------------')
+            if(response.status===200){
+                return true
+            } else {
+                return false
+            }
+        }, (error) => {
+            console.log(error);
+            return false
+        })
+    }
+
   }
   
   export default new Auth();
